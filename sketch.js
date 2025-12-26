0fe9c6abc222392a581e4ae1b3ea3aa785ac1ed2
Stephanie Bryan <stephanieebryan@gmail.com>
2025-12-25 21:48:00 -0800

    scaled to device

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@ let flozBtn;
 let tspBtn;
 let slider;
 let gui;
-//set initial waterHeight to 600;
-let waterHeight = 600;
+//set initial waterHeight to 400;
+let waterHeight = 400;
 let units = [];
 
 function preload() {
@@ -31,37 +31,37 @@ function setup() {
   units.push(new unitSystem(100, 10, 'mL', 1)); // scale is pixels per major tic unit (10mL here)
   units.push(new unitSystem(296, 1, 'fl oz', 0.03381));
   units.push(new unitSystem(49.3, 1, 'tsp', 0.203));
-  flozBtn = createButton("convert to oz", 50, 500);
-  tspBtn = createButton("convert to tsp", 50, 550);
-  slider = createSliderV("slider", 350, 300, 30, 300, 0, 350);
+  flozBtn = createButton("convert to oz", width / 10, width);
+  tspBtn = createButton("convert to tsp", width / 10, 1.1 * width);
+  slider = createSliderV("slider", 0.7 * width, 0.6 * width, 0.06 * width, width / 2, 0, 350 * width / 500);
 }
 
 function draw() {
   background(255);
-
-  image(waterPic, 117, waterHeight, 318, 415);
-  image(myCylinderPic, 50, 50, 400, 750);
+  //waterHeight = 600 * width / 500;
+  image(waterPic, 0.234 * width, waterHeight, 318 * width / 500, 415 * width / 500);
+  image(myCylinderPic, width / 10, width / 10, 0.8 * width, 1.5 * width);
 
   push();
-  textSize(20);
+  textSize(floor(0.04 * width));
   textFont(ComicNeue);
   textWrap(WORD);
   fill(0);
-  text("Use the slider on the right to change the amount of water in the graduated cylinder. Then hold down the buttons below to see this measurement in different units.", 50, 50, 450, 200);
+  text("Use the slider on the right to change the amount of water in the graduated cylinder. Then hold down the buttons below to see this measurement in different units.", width / 10, width / 10, 0.9 * width, 0.4 * width);
   pop();
 
   // Add a background bar at the bottom
   push();
   fill(255);
   noStroke();
-  rect(0, 780, 500, 120);
+  rect(0, 1.56 * width, width, 0.24 * width);
   pop();
 
   drawGui();
+  let waterMin = 642 * height / 900;
+  waterHeight = waterMin - slider.val;
 
-  waterHeight = 642 - slider.val;
-
-  let waterAmount = 10 + (642 - waterHeight) * 0.1; // Calculating water amount (10mL per 100px)
+  let waterAmount = 10 + (slider.val * 500 / width) * 0.1; // Calculating water amount (10mL per 100px)
   let i;
 
   if (flozBtn.isHeld) {
@@ -84,6 +84,6 @@ function draw() {
 
 function touchMoved() {
   // Optional: Add specific touch interaction logic here if needed
-  //return false; // This line is key to stopping default scrolling
+  return false; // This line is key to stopping default scrolling
 }
 
--- unitSystem.js
+++ unitSystem.js
@@ -13,17 +13,17 @@ class unitSystem {
         for (let i = 0; i < this.max; i++) {
             let largeTic = 760 - this.scale * (i + 1);
             let largeText = this.text * (i + 1);
-            textSize(16);
+            textSize(floor(16 * width / 500));
             textFont(Chewy);
             if (largeTic < 690) {
-                rect(295, largeTic, 18, 2); // Draw major ticks
-                text(largeText, 275, largeTic + 5); // Display unit labels
+                rect(295 * width / 500, largeTic * width / 500, 18 * width / 500, 2); // Draw major ticks
+                text(largeText, 275 * width / 500, (largeTic + 5) * width / 500); // Display unit labels
             }
             // Draw small ticks for finer granularity
             for (let j = 0; j < 10; j++) {
                 let smallTic = largeTic + (j + 1) * this.scale / 10;
                 if (smallTic < 690) {
-                    rect(305, smallTic, 8, 1); // Draw small ticks
+                    rect(305 * width / 500, smallTic * width / 500, 8 * width / 500, 1); // Draw small ticks
                 }
             }
         }
@@ -34,7 +34,7 @@ class unitSystem {
             for (let j = 0; j < 10; j++) {
                 let smallTic = (760 - this.scale * (this.max + 1)) + (j + 1) * this.scale / 10;
                 if (smallTic > 200) {
-                    rect(305, smallTic, 8, 1); // Draw extra small ticks
+                    rect(305 * width / 500, smallTic * width / 500, 8 * width / 500, 1); // Draw extra small ticks
                 }
             }
         }
@@ -57,7 +57,7 @@ class unitSystem {
 
         // Display the converted amount with the unit
         fill(0);
-        text(formattedAmount + " " + this.unit, 50, 400);
+        text(formattedAmount + " " + this.unit, width / 10, 0.8 * width);
 
     }
 }
